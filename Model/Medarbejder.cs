a92eefd74d5b4ece73ccedcd8dc81998d995061f
Lasse Sigh Klitgaard <lass6268@edu.eal.dk>
2018-04-18 12:35:21 +0200

    oprettet projekt og medarbjder

---
+++ Model/Medarbejder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Medarbejder
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+
+
+        public Medarbejder(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
---
+++ Model/Projekt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Projekt
+    {
+
+        public string Name { get; set; }
+        public int MinBudget { get; set; }
+        public int MaxBudget { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinalDate { get; set; }
+
+        public Projekt(string _Name, int _minBudget, int _maxBudget, DateTime startdate, DateTime _finaldate)
+        {
+            Name = _Name;
+            MinBudget = _minBudget;
+            MaxBudget = _maxBudget;
+            StartDate = startdate;
+            FinalDate = _finaldate;
+        }
+
+
+
+
+
+
+    }
+}
