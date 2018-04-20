0b610a8c265591d7af76fd1000069efeb6ee55d9
Lasse Sigh Klitgaard <lass6268@edu.eal.dk>
2018-04-20 14:16:52 +0200

    rettet DB communicator

--- ViewModel/DBcommunicator.cs
+++ ViewModel/DBcommunicator.cs
@@ -12,21 +12,21 @@ namespace ViewModel
         DbConcection dbConcection = new DbConcection();
         public void AddProjectToDatabase(Project project)
         {
-            dbConcection.AddProject(project.ProjectName, project.MinBudget, project.MaxBudget, project.StartDate, project.FinalDate);
+            dbConcection.AddProject(project.ProjectName, project.MinBudget, project.MaxBudget, project.StartDate, project.FinishDate);
 
         }
 
 
         public void ArchiveProjectToDatabase(Project project)
         {
-            dbConcection.ArchiveProject(project.ProjectName, project.MinBudget, project.MaxBudget, project.StartDate, project.FinalDate);
+            dbConcection.ArchiveProject(project.ProjectName, project.MinBudget, project.MaxBudget, project.StartDate,project.FinishDate);
 
         }
 
 
         public void AddBudgetToDatabase(Budget budget)
         {
-           dbConcection.AddBudget(budget.BudgetTotal, budget.CurrentBudget, budget.BudgetStartdate, budget.BudgetFinishdate);
+           //dbConcection.AddBudget(budget.BudgetTotal, budget.CurrentBudget, budget.BudgetStartdate, budget.BudgetFinishdate);
           
         }
 
