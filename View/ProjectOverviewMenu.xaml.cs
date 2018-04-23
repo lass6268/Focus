3393c7f54630e769d144632f7dc885aa93c9f41b
Lasse Sigh Klitgaard <lass6268@edu.eal.dk>
2018-04-23 13:44:45 +0200

    Projektcollection lavet Generic list til observable

--- View/ProjectOverviewMenu.xaml.cs
+++ View/ProjectOverviewMenu.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace View
 {
@@ -22,6 +23,9 @@ namespace View
         public ProjectOverviewMenu()
         {
             InitializeComponent();
+            DataContext = new ProjektCollection();
         }
+
+       
     }
 }
