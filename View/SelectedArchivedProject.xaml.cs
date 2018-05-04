fecff690efa5b605f780b6f2fadfb099fc566042
Andreas Nielsen <andr522f@edu.eal.dk>
2018-05-04 10:57:10 +0200

    budget virker på "SelectedArchivedProject"

--- View/SelectedArchivedProject.xaml.cs
+++ View/SelectedArchivedProject.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
